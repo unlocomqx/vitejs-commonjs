b0406681759dcc1883cd3d0512c39ba38dccc6b0
unlocomqx <amxmodx.unloco@gmail.com>
2021-07-11 12:16:06 +0100

    Empty config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
-
 /**
  * @type {import('vite').UserConfig}
  */
 const config = {
-  plugins: [
-    viteCommonjs(),
-  ]
+
 };
 
 export default config;
